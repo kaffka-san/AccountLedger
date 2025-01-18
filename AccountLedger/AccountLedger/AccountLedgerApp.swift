123a9c9f7692614ae981185e71f8234b6780acc9
Anastasia Lenina <anastasia.lenina@applifting.cz>
2025-01-18 14:15:53 +0100

    initial project

---
+++ AccountLedger/AccountLedger/AccountLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountLedgerApp.swift
+//  AccountLedger
+//
+//  Created by Anastasia Lenina on 18.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AccountLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccountLedger/AccountLedgerTests/AccountLedgerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AccountLedgerTests.swift
+//  AccountLedgerTests
+//
+//  Created by Anastasia Lenina on 18.01.2025.
+//
+
+import Testing
+
+struct AccountLedgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
