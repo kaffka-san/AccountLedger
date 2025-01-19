af9167f5064e70fd0430822366edc58cd1a7ef24
Anastasia Lenina <anastasia.lenina@applifting.cz>
2025-01-19 12:22:39 +0100

    add components to the AccountListView

--- AccountLedger/AccountLedger/UI Layer/Screens/AccountList/AccountListView.swift
+++ AccountLedger/AccountLedger/UI Layer/Screens/AccountList/AccountListView.swift
@@ -9,12 +9,77 @@ import SwiftUI
 
 struct AccountListView: View {
     @ObservedObject var viewModel = AccountListViewModel() // Dependency injection will be introduced later
+    @State private var selectedAccountId: String?
     
     var body: some View {
-        Text("Account List")
+        content()
             .onAppear {
                 viewModel.fetchAccounts()
             }
+    }
+}
+
+extension AccountListView {
+    @ViewBuilder
+    func content() -> some View {
+        NavigationStack {
+            ZStack {
+                contentList()
+                progressView()
+            }
+            .accentColor(.gray)
+            .navigationTitle("Accounts")
+        }
+    }
+    
+    @ViewBuilder
+    func contentList() -> some View {
+        List(viewModel.accounts) { account in
+            row(for: account)
+        }
+        .listStyle(.automatic)
+    }
+    
+    @ViewBuilder
+    func row(for account: TransparentAccount) -> some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 8) {
+                accountName(account.name)
+                accountNumber(account.accountNumber)
+            }
+            Spacer()
+            balance(balance: account.balance, currency: account.currency)
+        }
+        .padding(.vertical)
+        .contentShape(Rectangle())
+        
+    }
+    
+    @ViewBuilder
+    func accountName(_ name: String) -> some View {
+        Text(name)
+            .font(.system(size: 12, weight: .black))
+            .foregroundStyle(.black.opacity(0.7))
+    }
+    
+    @ViewBuilder
+    func accountNumber(_ number: String) -> some View {
+        Text(number)
+            .font(.system(size: 12, weight: .black))
+            .foregroundStyle(.black.opacity(0.2))
+    }
+    
+    @ViewBuilder
+    func balance(balance: Double, currency: String?) -> some View {
+        Text("\(balance, specifier: "%.2f") \(currency ?? "")")
+            .font(.system(size: 14, weight: .black))
+    }
+    
+    @ViewBuilder
+    func progressView() -> some View {
+        if viewModel.isLoading {
+            ProgressView()
+        }
     }
 }
 
