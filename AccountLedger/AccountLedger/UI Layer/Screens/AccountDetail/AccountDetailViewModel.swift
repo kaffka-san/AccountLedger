f4bae64d1dc35805cd879cbf99ce04be74f03313
Anastasia Lenina <anastasia.lenina@applifting.cz>
2025-01-20 21:12:11 +0100

    add data parsing

--- AccountLedger/AccountLedger/UI Layer/Screens/AccountDetail/AccountDetailViewModel.swift
+++ AccountLedger/AccountLedger/UI Layer/Screens/AccountDetail/AccountDetailViewModel.swift
@@ -18,10 +18,35 @@ final class AccountDetailViewModel: ObservableObject {
     }
 }
 
+// MARK: - Public properties
+extension AccountDetailViewModel {
+    var monthlyCashFlow: [(month: String, total: Double)] {
+        Dictionary(grouping: transactions) { transaction in
+            transaction.processingDate.formatAsMonthYear()
+        }
+        .map { (month, transactions) in
+            let total = transactions.reduce(0) { $0 + $1.amount.value }
+            return (month: month, total: total)
+        }
+        .sorted { $0.0 < $1.0 }
+    }
+    
+    var maxValueWithBuffer: Double {
+        let max = monthlyCashFlow.map { $0.total }.max() ?? 0
+        return max + (0.2 * max)
+    }
+
+    var minValueWithBuffer: Double {
+        let min = monthlyCashFlow.map { $0.total }.min() ?? 0
+        return min - (0.2 * min)
+    }
+}
+
 // MARK: - Public methods
 extension AccountDetailViewModel {
     @MainActor
     func fetchTransactions() {
+        isLoading = true
         Task { [weak self] in
             guard let self else { return }
             do {
